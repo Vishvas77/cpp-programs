3dcbdba1d5828fa12fe3709d4cddb772d4e28f46
Vishvas77 <polepakavishvas@gmail.com>
2026-02-17 17:08:46 +0530

    list of strings, perform insertion and deletion, and traverse using an iterator

---
+++ Data Structures/Syllabus/week3b.cpp
@@ -0,0 +1,23 @@
+/*Menu Driven to perform various vector operations*/
+#include <iostream>
+#include <list>
+using namespace std;
+
+int main()
+{
+   list<string>l;
+   l.push_back("C++");
+   l.push_back("Java");
+   l.push_back("Python");
+   cout << "Initial list:\n";
+   for(list<string>::iterator it=l.begin();it!=l.end();it++)
+   cout << *it <<"";
+
+   l.push_front("DS");
+   l.pop_back();
+
+   cout << "\n\nAfter Insertion and Deletion:\n";
+   for(list<string>::iterator it=l.begin();it!=l.end();it++)
+   cout << *it <<"";
+   return 0;
+}
